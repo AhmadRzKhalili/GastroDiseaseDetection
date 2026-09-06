039bc1bbf3c3512a679113a156ede2a35be1f39f
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2026-09-06 01:00:46 +0100

    import new functions

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,17 @@ from .utils import load_csv_to_df
 from .data_preprocessing import (
     analyse_table_features,
     analyse_numeric_features_range,
+    analyse_feature_variance,
+    calculate_feature_variance,
     get_rows_with_missing_data,
     encode_non_numeric_features,
     encode_categorical_features,
     explore_correlation,
     analyse_feature_correlation,
     explore_feature_correlation,
+    analyse_feature_dependency,
+    explore_feature_dependency,
+    calculate_vif,
+    calculate_cramers_v,
+    calculate_mutual_information,
 )
--- src/data_preprocessing/__init__.py
+++ src/data_preprocessing/__init__.py
@@ -1,7 +1,19 @@
-from .analyse_tabular_data import analyse_table_features, analyse_numeric_features_range
+from .analyse_tabular_data import (
+    analyse_table_features,
+    analyse_numeric_features_range,
+    analyse_feature_variance,
+    calculate_feature_variance,
+)
 from .cleanup_tabular_data import (
     get_rows_with_missing_data,
     encode_non_numeric_features,
     encode_categorical_features,
 )
 from .explore_correlation import explore_correlation, analyse_feature_correlation, explore_feature_correlation
+from .analyse_feature_dependency import (
+    analyse_feature_dependency,
+    explore_feature_dependency,
+    calculate_vif,
+    calculate_cramers_v,
+    calculate_mutual_information,
+)
